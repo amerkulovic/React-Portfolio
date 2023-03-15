bd5185e9506be47c3f13a2972cd8c52ca33f9346
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-15 12:21:59 -0500

    added styling and text to the Body

--- src/Body.js
+++ src/Body.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Body = () => {
   return (
-    <div>
-      <p>Hi, my name is Amer Kulovic. I create and design webpages.</p>
-      <p>Check out my work!</p>
+    <div className="px-10 py-12 h-screen">
+      <p className="font-bold text-6xl">Hi, my name is <span className="text-[#F97444]">Amer Kulovic</span>.<br></br>Software Engineer</p>
+      <p className="font-light">Check out my work!</p>
     </div>
   );
 };
