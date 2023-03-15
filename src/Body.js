c6d5f944223507db016ee0e201d7e2c9199669eb
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-15 14:02:58 -0500

    added images for footer links and styled

--- src/Body.js
+++ src/Body.js
@@ -7,8 +7,8 @@ const Body = () => {
         Hi, my name is <span className="text-[#F97444]">Amer Kulovic</span>.<br></br>Software Engineer
       </p>
       <p className="font-light px-10 text-lg">Check out my work!</p>
-      <div className="bg-[#032A5D] mt-80 p-10">
-        <h1 className="text-white font-bold text-4xl py-5">About Me</h1>
+      <section className="bg-[#032A5D] mt-80 p-10">
+        <h1 className="text-[#F97444] font-bold text-4xl py-5">About Me</h1>
         <p className="text-white font-semibold text-2xl mx-7 py-5">My name is Amer Kulovic. I am a Software Engineer! I have a certificate from Northwestern in Software Engineering and a degree in Business Management from Northeastern Illinois University. I am an active learner and love tackling new tasks. I am experienced in </p>
         <ul className="text-white font-semibold text-2xl py-5 inline-block w-6/12 m-0">
           <li>JavaScript</li>
@@ -21,7 +21,7 @@ const Body = () => {
           <li>jQuery</li>
         </ul>
         <p className="text-white font-semibold text-2xl mx-7 py-5">I have built multiple websites from scratch and I'm continuously learning more about the world of coding! Some personal hobbies of mine are playing basketball, weightlifting, and watching movies.</p>
-      </div>
+      </section>
     </div>
   );
 };
--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,17 @@
 import React from "react";
+import githubLogo from "./github.png";
+import linkedInLogo from "./linkedIn.png";
 
 const Footer = () => {
   return (
     <div>
-      <footer>
-        <a href="https://github.com/amerkulovic">GitHub</a>
-        <a href="https://www.linkedin.com/in/amer-kulovic/">LinkedIn</a>
+      <footer className="flex items-center justify-center py-5 space-x-5">
+        <a href="https://github.com/amerkulovic" target="_blank">
+          <img src={githubLogo} width="50" height="50"></img>
+        </a>
+        <a href="https://www.linkedin.com/in/amer-kulovic/" target="_blank">
+          <img src={linkedInLogo} width="50" height="50"></img>
+        </a>
       </footer>
     </div>
   );
