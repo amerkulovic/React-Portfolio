9e16d802322c7832f863848606e2da7d1b8311f9
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-15 11:06:10 -0500

    added styling and imported tailwinds to index.css

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+
 const Header = () => {
   return (
-    <div>
-        <h1 className="text-red-300">Amer</h1>
-      <nav>
+    <div className="flex flex-row">
+      <h1 className="text-red-300 text-5xl"><span className="text-red-700">A</span>mer</h1>
+      <nav className="px-2">
         <a>About Me</a>
         <a>Portfolio</a>
         <a>Contact</a>
