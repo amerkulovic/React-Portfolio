af88ebaaf349958ca48fe3a981e4f2e401d01889
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-15 11:52:51 -0500

    added more styling to header

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-
 const Header = () => {
   return (
-    <div className="flex flex-row">
-      <h1 className="text-red-300 text-5xl"><span className="text-red-700">A</span>mer</h1>
-      <nav className="px-2">
-        <a>About Me</a>
-        <a>Portfolio</a>
-        <a>Contact</a>
+    <div className="flex flex-row py-5 justify-between">
+      <h1 className="text-purple-800 text-5xl px-5">
+        &lt;Ame<span className="text-purple-500">r /&gt;</span>
+      </h1>
+      <nav className="px-8 flex items-center text-black space-x-5">
+        <a className="font-bold">About Me</a>
+        <a className="font-bold">Portfolio</a>
+        <a className="text-white bg-purple-800 font-bold p-3 rounded-3xl">Contact Me</a>
       </nav>
     </div>
   );
