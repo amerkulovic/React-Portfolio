71baf85f3ad40669a5ac9c78d1373f29ee4a52ee
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-15 23:36:49 -0500

    created a Project component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Project from "./Project";
 import myMovieSpace from "../images/MyMovieSpace.png";
 import indianaJones from "../images/IndianaJones.png";
 import futureFinder from "../images/FutureFinder.png";
@@ -12,72 +13,12 @@ const Portfolio = () => {
     <section className="bg-[#DAD9D5] p-10">
       <h1 className="text-[#032A5D] font-bold text-4xl py-5">Portfolio</h1>
       <div className="inline-block">
-        <button>
-          <a href="https://grouponeel.github.io/01_group-project/" target="_blank">
-            <div className="container">
-              <img src={myMovieSpace} className="work-picture"></img>
-              <h1 className="picture-text">MyMovieSpace</h1>
-              <a className="github-text" href="https://github.com/GroupOneEl/01_group-project" target="_blank">
-                <img src={githubLogo} width="50" height="50"></img>
-              </a>
-            </div>
-          </a>
-        </button>
-        <button>
-          <a href="https://amerkulovic.github.io/Indy-Jones-Questionaire/" target="_blank">
-            <div className="container">
-              <img src={indianaJones} className="work-picture"></img>
-              <h1 className="picture-text">Indiana Jones Quiz</h1>
-              <a className="github-text" href="https://github.com/amerkulovic/Indy-Jones-Questionaire" target="_blank">
-                <img src={githubLogo} width="50" height="50"></img>
-              </a>
-            </div>
-          </a>
-        </button>
-        <button>
-          <a href="https://future-finder.herokuapp.com/" target="_blank">
-            <div className="container">
-              <img src={futureFinder} className="work-picture"></img>
-              <h1 className="picture-text">Future Finder</h1>
-              <a className="github-text" href="https://github.com/4-non-non-dorks/future-finder" target="_blank">
-                <img src={githubLogo} width="50" height="50"></img>
-              </a>
-            </div>
-          </a>
-        </button>
-        <button>
-          <a href="https://amerkulovic.github.io/Weather-Dashboard/ " target="_blank">
-            <div className="container">
-              <img src={weatherDashboard} className="work-picture"></img>
-              <h1 className="picture-text">Weather Board</h1>
-              <a className="github-text" href="https://github.com/amerkulovic/Weather-Dashboard" target="_blank">
-                <img src={githubLogo} width="50" height="50"></img>
-              </a>
-            </div>
-          </a>
-        </button>
-        <button>
-          <a href="https://www.youtube.com/watch?v=QZSxZ1aRiGQ" target="_blank">
-            <div className="container">
-              <img src={socialNetwork} className="work-picture"></img>
-              <h1 className="picture-text">Social Network API</h1>
-              <a className="github-text" href="https://github.com/amerkulovic/Social-Network-API" target="_blank">
-                <img src={githubLogo} width="50" height="50"></img>
-              </a>
-            </div>
-          </a>
-        </button>
-        <button>
-          <a href="https://note-taker-ak2.herokuapp.com/" target="_blank">
-            <div className="container">
-              <img src={noteTaker} className="work-picture"></img>
-              <h1 className="picture-text">Note Taker</h1>
-              <a className="github-text" href="https://github.com/amerkulovic/Note-Taker" target="_blank">
-                <img src={githubLogo} width="50" height="50"></img>
-              </a>
-            </div>
-          </a>
-        </button>
+        <Project link="https://grouponeel.github.io/01_group-project/" imgLink={myMovieSpace} githubLink="https://github.com/GroupOneEl/01_group-project" projectTitle="MyMovieSpace" />
+        <Project link="https://amerkulovic.github.io/Indy-Jones-Questionaire/" imgLink={indianaJones} githubLink="https://github.com/amerkulovic/Indy-Jones-Questionaire" projectTitle="Indiana Jones Quiz" />
+        <Project link="https://future-finder.herokuapp.com/" imgLink={futureFinder} githubLink="https://github.com/4-non-non-dorks/future-finder" projectTitle="Future Finder" />
+        <Project link="https://amerkulovic.github.io/Weather-Dashboard/ " imgLink={weatherDashboard} githubLink="https://github.com/amerkulovic/Weather-Dashboard" projectTitle="Weather Board" />
+        <Project link="https://www.youtube.com/watch?v=QZSxZ1aRiGQ" imgLink={socialNetwork} githubLink="https://github.com/amerkulovic/Social-Network-API" projectTitle="Social Network API" />
+        <Project link="https://note-taker-ak2.herokuapp.com/" imgLink={noteTaker} githubLink="https://github.com/amerkulovic/Note-Taker" projectTitle="Note Taker" />
       </div>
     </section>
   );
---
+++ src/components/Project.js
@@ -0,0 +1,20 @@
+import React from "react";
+import githubLogo from "../images/github-white.png";
+
+const Project = (props) => {
+  return (
+    <button>
+      <a href={props.link} target="_blank">
+        <div className="container">
+          <img src={props.imgLink} className="work-picture"></img>
+          <h1 className="picture-text">{props.projectTitle}</h1>
+          <a className="github-text" href={props.githubLink} target="_blank">
+            <img src={githubLogo} width="50" height="50"></img>
+          </a>
+        </div>
+      </a>
+    </button>
+  );
+};
+
+export default Project;
