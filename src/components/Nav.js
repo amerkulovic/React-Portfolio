8e9df11f67888734d313cd49b3d424fb3c75f0a5
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-17 11:05:37 -0500

    converted the nav bar to an unordered list

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,25 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <nav className="px-8 flex items-center text-black space-x-5">
-      <button>
-        <a href="#about" className="font-bold underline-hover-effect">About Me</a>
-      </button>
-      <button>
-        <a href="#portfolio" className="font-bold underline-hover-effect">Portfolio</a>
-      </button>
-      <button>
-        <a className="font-bold underline-hover-effect">Resume</a>
-      </button>
-      <button>
-        <a className="text-white bg-[#032A5D] font-bold py-3 px-4 rounded-3xl hover:shadow-2xl">Contact</a>
-      </button>
+    <nav>
+      <ul className="px-8 flex items-center text-black space-x-5">
+        <li>
+          <a href="#about" className="font-bold underline-hover-effect">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio" className="font-bold underline-hover-effect">
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a className="font-bold underline-hover-effect">Resume</a>
+        </li>
+        <li>
+          <a className="text-white bg-[#032A5D] font-bold py-3 px-4 rounded-3xl hover:shadow-2xl">Contact</a>
+        </li>
+      </ul>
     </nav>
   );
 };
