9ded968f637e27b3dc26e2e90fa3eec8d235cedb
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-18 12:39:27 -0500

    started styling the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,24 @@
 const Contact = () => {
   return (
-    <div className="h-screen">
-        <h1>Where to Contact Me</h1>
-        <h1>Email: amer.kulovic@yahoo.com</h1>
-        <h1>Phone Number: 7773-600-1897</h1>
+    <div className="h-screen text-center">
+      <form className="bg-[#CCDAEB] inline-block text-center p-10">
+        <h1 className="font-bold text-[#032a5d]">Contact Me!</h1>
+        <div className="py-3 flex flex-col">
+          <label className="text-[#032a5d]">Name:</label>
+          <input type="text" />
+        </div>
+        <div className="py-3 flex flex-col">
+          <label className="text-[#032a5d]">Email:</label>
+          <input type="text" />
+        </div>
+        <div className="py-3 flex flex-col">
+          <label className="text-[#032a5d]">Message:</label>
+          <textarea name="message"></textarea>
+        </div>
+        <button className="bg-[#f97444] text-white px-3 py-2 rounded-lg">Submit</button>
+      </form>
     </div>
-  )
+  );
 };
 
 export default Contact;
