0a254a7ff2ce4e63dbbd30d72268b88993e726f2
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-18 20:33:22 -0500

    made progress towards input logic for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,48 @@
+import { useState } from "react";
+import "./Body.css";
+
 const Contact = () => {
+  let nameStyling = "";
+  const [nameInput, setNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+
+  const nameInputHandler = (event) => {
+    setNameInput(event.target.value);
+  };
+
+  const emailInputHandler = (event) => {
+    setEmailInput(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (nameInput === "" || emailInput === "") {
+      console.log("One field is empty");
+      nameStyling += "border border-red-600";
+    } else {
+      console.log("GOOD JOB, THE FIELDs WERE FILLED!");
+    }
+  };
   return (
     <div className="h-screen text-center">
-      <form className="bg-[#CCDAEB] inline-block text-center p-10">
+      <form className="bg-[#CCDAEB] m-5 text-center p-10">
         <h1 className="font-bold text-[#032a5d]">Contact Me!</h1>
         <div className="py-3 flex flex-col">
           <label className="text-[#032a5d]">Name:</label>
-          <input type="text" />
+          <input type="text" className={nameStyling} onChange={nameInputHandler} />
         </div>
         <div className="py-3 flex flex-col">
           <label className="text-[#032a5d]">Email:</label>
-          <input type="text" />
+          <input type="text" onChange={emailInputHandler} />
         </div>
         <div className="py-3 flex flex-col">
           <label className="text-[#032a5d]">Message:</label>
-          <textarea name="message"></textarea>
+          <textarea rows="5" cols="40" name="message"></textarea>
         </div>
-        <button className="bg-[#f97444] text-white px-3 py-2 rounded-lg">Submit</button>
+        <button onClick={submitHandler} className="bg-[#f97444] text-white px-3 py-2 rounded-lg">
+          Submit
+        </button>
       </form>
     </div>
   );
