c317b8b5895e3fe5347ddc8ba2622abd01de6718
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-27 18:10:39 -0500

    fixed bug with the download button

--- src/components/pageComponents/Resume.js
+++ src/components/pageComponents/Resume.js
@@ -1,26 +1,16 @@
 import resumeSvg from "../../images/resume.svg";
+
 import "./Resume.css";
 
 const Resume = () => {
-  const onButtonClick = () => {
-    fetch("ATS Resume.docx").then((response) => {
-      response.blob().then((blob) => {
-        const fileURL = window.URL.createObjectURL(blob);
-        let alink = document.createElement("a");
-        alink.href = fileURL;
-        alink.download = "ATS Resume.docx";
-        alink.click();
-      });
-    });
-  };
   return (
     <div className="flex flex-wrap items-center justify-center">
       <img className="py-5" src={resumeSvg}></img>
       <div className="text-center p-8">
         <h1 className="font-bold text-5xl mb-14">My Resume</h1>
-        <button onClick={onButtonClick} className="font-bold text-4xl border-4 border-[#032a5d] p-5 rounded-lg text-white download-btn">
-          Download
-        </button>
+        <a href="/Updated ATS Resume.pdf" download="Resume">
+          <button className="font-bold text-4xl border-4 border-[#032a5d] p-5 rounded-lg text-white download-btn">Download</button>
+        </a>
       </div>
     </div>
   );
