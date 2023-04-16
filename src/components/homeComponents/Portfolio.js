473ea276c357849af65fff751257beacddc2483b
Amer Kulovic <amer.kulovic@yahoo.com>
2023-04-15 23:27:34 -0500

    added a new project to display in portfolio

--- src/components/homeComponents/Portfolio.js
+++ src/components/homeComponents/Portfolio.js
@@ -6,7 +6,7 @@ import futureFinder from "../../images/FutureFinder.png";
 import socialNetwork from "../../images/SocialNetwork.png";
 import noteTaker from "../../images/NoteTaker.png";
 import weatherDashboard from "../../images/WeatherDashboard.png";
-import githubLogo from "../../images/github-white.png";
+import oceanBuddies from "../../images/OceanBuddies.png";
 
 const Portfolio = () => {
   return (
@@ -14,11 +14,11 @@ const Portfolio = () => {
       <h1 className="text-[#032A5D] font-bold text-4xl py-5">Portfolio</h1>
       <div className="flex flex-wrap justify-center">
         <Project link="https://grouponeel.github.io/01_group-project/" imgLink={myMovieSpace} githubLink="https://github.com/GroupOneEl/01_group-project" projectTitle="MyMovieSpace" />
-        <Project link="https://amerkulovic.github.io/Indy-Jones-Questionaire/" imgLink={indianaJones} githubLink="https://github.com/amerkulovic/Indy-Jones-Questionaire" projectTitle="Indiana Jones Quiz" />
+        <Project link="https://ocean-buddies.herokuapp.com/" imgLink={oceanBuddies} githubLink="https://github.com/Oceans-4/Ocean-App" projectTitle="Ocean Buddies" />
         <Project link="https://future-finder.herokuapp.com/" imgLink={futureFinder} githubLink="https://github.com/4-non-non-dorks/future-finder" projectTitle="Future Finder" />
+        <Project link="https://amerkulovic.github.io/Indy-Jones-Questionaire/" imgLink={indianaJones} githubLink="https://github.com/amerkulovic/Indy-Jones-Questionaire" projectTitle="Indiana Jones Quiz" />
         <Project link="https://amerkulovic.github.io/Weather-Dashboard/ " imgLink={weatherDashboard} githubLink="https://github.com/amerkulovic/Weather-Dashboard" projectTitle="Weather Board" />
         <Project link="https://www.youtube.com/watch?v=QZSxZ1aRiGQ" imgLink={socialNetwork} githubLink="https://github.com/amerkulovic/Social-Network-API" projectTitle="Social Network" />
-        <Project link="https://note-taker-ak2.herokuapp.com/" imgLink={noteTaker} githubLink="https://github.com/amerkulovic/Note-Taker" projectTitle="Note Taker" />
       </div>
     </section>
   );
