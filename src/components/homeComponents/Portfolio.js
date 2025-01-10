95a2dfc0d93c942b21961871a73efae9e9b44186
Amer Kulovic <amer.kulovic@yahoo.com>
2025-01-10 10:01:40 -0600

    updated project to contain most recent version of MyMovieSpace

--- src/components/homeComponents/Portfolio.js
+++ src/components/homeComponents/Portfolio.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Project from "./Project";
-import myMovieSpace from "../../images/MyMovieSpace.png";
+import myMovieSpace from "../../images/mymoviespace2.0.png";
 import indianaJones from "../../images/IndianaJones.png";
 import futureFinder from "../../images/FutureFinder.png";
 import socialNetwork from "../../images/SocialNetwork.png";
@@ -14,10 +14,10 @@ const Portfolio = () => {
     <section id="portfolio" className="bg-[#DAD9D5] p-10">
       <h1 className="text-[#032A5D] font-bold text-4xl py-5">Portfolio</h1>
       <div className="flex flex-wrap justify-center">
-        <Project link="https://grouponeel.github.io/01_group-project/" imgLink={myMovieSpace} githubLink="https://github.com/GroupOneEl/01_group-project" projectTitle="MyMovieSpace" />
+        <Project link="https://my-movie-space-9d82abf6cb80.herokuapp.com/" imgLink={myMovieSpace} githubLink="https://github.com/amerkulovic/MyMovieSpace2" projectTitle="MyMovieSpace" />
         <Project link="https://ocean-buddies.herokuapp.com/" imgLink={oceanBuddies} githubLink="https://github.com/Oceans-4/Ocean-App" projectTitle="Ocean Buddies" />
         <Project link="https://amerkulovic.github.io/NovelNest/" imgLink={novelNest} githubLink="https://github.com/amerkulovic/NovelNest" projectTitle="Novel Nest" />
-        <Project link="https://future-finder.herokuapp.com/" imgLink={futureFinder} githubLink="https://github.com/4-non-non-dorks/future-finder" projectTitle="Future Finder" />
+        <Project link="https://future-finder.herokuapp.com" imgLink={futureFinder} githubLink="https://github.com/4-non-non-dorks/future-finder" projectTitle="Future Finder" />
         <Project link="https://amerkulovic.github.io/Indy-Jones-Questionaire/" imgLink={indianaJones} githubLink="https://github.com/amerkulovic/Indy-Jones-Questionaire" projectTitle="Indiana Jones Quiz" />
         <Project link="https://amerkulovic.github.io/Weather-Dashboard/ " imgLink={weatherDashboard} githubLink="https://github.com/amerkulovic/Weather-Dashboard" projectTitle="Weather Board" />
       </div>
